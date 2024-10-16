2ee9df3a2d265d91235ec9d3e56f94c120b83b87
Seo-Woongbi <suwoongby@naver.com>
2024-10-16 13:04:15 +0900

    2일차 구현중

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI currentScoreTxt;
+    [SerializeField] private TextMeshProUGUI HighScoreTxt;
+
+    int score = 0;
+    Rocket rocket;
+
     private Rigidbody2D rb2d;
     private float fuel = 100f;
     
@@ -12,8 +20,9 @@ public class Rocket : MonoBehaviour
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
         rb2d = GetComponent<Rigidbody2D>();
+        rocket = GetComponent<Rocket>();
     }
-    
+
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
@@ -23,5 +32,26 @@ public class Rocket : MonoBehaviour
             rb2d.AddForce(Vector2.up * SPEED);
         }
         else return;
+    }
+
+    public void AddScore()
+    {
+        score = (int)rocket.transform.position.y;
+        currentScoreTxt.text = $"{score} M";
+    }
+
+    private void Update()
+    {
+        AddScore();
+    }
+
+    private void HighScore()
+    { 
+
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
