db3ebb1175bc479edc3140f794590ecea16cfa99
Seo-Woongbi <suwoongby@naver.com>
2024-10-18 12:22:08 +0900

    로켓 관련 스크립트 분할

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,11 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI currentScoreTxt;
-    [SerializeField] private TextMeshProUGUI HighScoreTxt;
-
-    int score = 0;
-    Rocket rocket;
+    RocketDashBoard dashBoard;
 
     private Rigidbody2D rb2d;
     private float fuel = 100f;
@@ -16,16 +12,12 @@ public class Rocket : MonoBehaviour
     private readonly float SPEED = 500f;
     private readonly float FUELPERSHOOT = 10f;
 
-    string highScoreStr = "HighScore";
-    int highscore;
+   
 
     void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
-        rb2d = GetComponent<Rigidbody2D>();
-        rocket = GetComponent<Rocket>();
-        GetHighScore();
-        HighScoreTxt.text = $"HighScore : {highscore} M";
+        rb2d = GetComponent<Rigidbody2D>();;
     }
 
     public void Shoot()
@@ -37,37 +29,5 @@ public class Rocket : MonoBehaviour
             rb2d.AddForce(Vector2.up * SPEED);
         }
         else return;
-    }
-
-    public void AddScore()
-    {
-        score = (int)rocket.transform.position.y;
-        currentScoreTxt.text = $" {score} M";
-    }
-
-    private void Update()
-    {
-        AddScore();
-        SetHighScore();
-    }
-
-    private int GetHighScore()
-    {
-        highscore = PlayerPrefs.GetInt(highScoreStr);
-        return highscore;
-    }
-
-    private void SetHighScore()
-    {
-        if(score > GetHighScore())
-        {
-            PlayerPrefs.SetInt(highScoreStr, score);
-        }
-
-    }
-
-    public void RetryButton()
-    {
-        SceneManager.LoadScene("SampleScene");
     }
 }
---
+++ Assets/Scripts/RocketDashBoard.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Sockets;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine;
+using TMPro;
+
+public class RocketDashBoard
+{
+    [SerializeField] private TextMeshProUGUI currentScoreTxt;
+    [SerializeField] private TextMeshProUGUI HighScoreTxt;
+
+    int score = 0;
+    Rocket rocket;
+
+    string highScoreStr = "HighScore";
+    int highscore;
+
+    private void Awake()
+    {
+        GetHighScore();
+        HighScoreTxt.text = $"HighScore : {highscore} M";
+    }
+
+    public void AddScore()
+    {
+        score = (int)rocket.transform.position.y;
+        currentScoreTxt.text = $" {score} M";
+    }
+
+    public int GetHighScore()
+    {
+        highscore = PlayerPrefs.GetInt(highScoreStr);
+        return highscore;
+    }
+
+    private void SetHighScore()
+    {
+        if (score > GetHighScore())
+        {
+            PlayerPrefs.SetInt(highScoreStr, score);
+        }
+
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private void Update()
+    {
+        AddScore();
+        SetHighScore();
+    }
+
+}
